917c85b191a59c22d4b21b23cf89d7998008e6ae
crschung <60279042+crschung@users.noreply.github.com>
2020-08-05 21:38:46 -0700

    update links

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function playBGM(){
-    var audio = new Audio('music/Our Moment - Full Lyrics Video Persona 3 Dancing Moon Night.mp3');
-    if(audio.paused){
-      audio.play();
+    var myAudio = new Audio('music/Our Moment - Full Lyrics Video Persona 3 Dancing Moon Night.mp3');
+    if(myAudio.duration > 0 || !myAudio.paused){
+      myAudio.pause();
+    }
+    else{
+      myAudio.play();
     }
 }
