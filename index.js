25c3c157f8f6fcf9075d73754552b53fbfc0c32a
crschung <60279042+crschung@users.noreply.github.com>
2021-06-04 21:31:32 -0700

    feat: update html/css

--- index.js
+++
@@ -1,9 +0,0 @@
-function playBGM(){
-    var myAudio = new Audio('music/Our Moment - Full Lyrics Video Persona 3 Dancing Moon Night.mp3');
-    if(myAudio.duration > 0 || !myAudio.paused){
-      myAudio.pause();
-    }
-    else{
-      myAudio.play();
-    }
-}
